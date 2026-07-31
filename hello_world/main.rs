81c647cf037b121f73cc61da2b82659bdf701d5a
The Professor <caspianvale@proton.me>
2026-07-31 23:06:02 +0530

    first hello world program in rust

---
+++ hello_world/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    // both are macros in Rust, 
+    // anything ending with a ! mark is a macro, 
+    // think of macros as code that writes other code.
+    println!("Hello World"); // appends a new line character (\n) at the end of the text
+    print!("Hello World"); // doesn't append  a new line
+}
