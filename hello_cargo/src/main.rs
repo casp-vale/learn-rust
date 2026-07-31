834a72423436a9e1bc3150c0ec18267aa7b5cea6
The Professor <caspianvale@proton.me>
2026-07-31 23:12:18 +0530

    hello world using cargo

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+    // create a project using 'cargo new'.
+    // build a project using 'cargo build'.
+    // build and run a project in one step using 'cargo run'.
+    // build a project without producing a binary to check for errors using 'cargo check'.
+    // cargo stores the build in the 'target/debug' directory.
+    println!("Hello, world!");
+}
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/professor/Work/rust/learn-rust/hello_cargo/target/debug/hello_cargo: /home/professor/Work/rust/learn-rust/hello_cargo/src/main.rs
