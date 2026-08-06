f302e441491791f149f0c2c28f2b0a5df1735093
The Professor <caspianvale@proton.me>
2026-08-06 09:22:31 +0530

    references and borrowing

---
+++ references_borrowing/src/main.rs
@@ -0,0 +1,43 @@
+// fn main() {
+//     let s1 = String::from("hello");
+
+//     let len = calculate_length(&s1);
+
+//     println!("The length of '{s1}' is {len}.");
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+
+// fn main() {
+//     let s = String::from("hello");
+
+//     change(&s);
+// }
+
+// fn change(some_string: &String) {
+//     some_string.push_str(", world");
+// }
+
+// fn main() {
+//     let mut s = String::from("hello");
+
+//     change(&mut s);
+// }
+
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{r1} and {r2}");
+    // Variables r1 and r2 will not be used after this point.
+
+    let r3 = &mut s; // no problem
+    println!("{r3}");
+}
